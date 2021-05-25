db9945c121f62753666b9d6e4ecadaa4c183b66f
julian <pkarki.developer@gmail.com>
2021-05-25 07:42:17 -0400

    Adding swagger to the project

--- src/main/java/com/bah/starter/Application.java
+++ src/main/java/com/bah/starter/Application.java
@@ -1,13 +1,45 @@
 package com.bah.starter;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.mappers.ModelMapper;
+
+@Slf4j
 @SpringBootApplication
-public class Application {
+@Configuration
+@EnableSwagger2
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// log.info("Application Started");
+		// TODO Auto-generated method stub
+		System.out.println("Application started");
+
+	}
+	/*
+	 * @Bean public ModelMapper modelMapper() { return new ModelMapper();
+	 */
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+
 	}
 
 }
