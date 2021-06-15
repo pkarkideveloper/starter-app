6f6e0cb3ac6a19bcc2c4d24c06383f12bc86e537
unknown <pkarki.developer@gmail.com>
2021-06-15 08:49:59 -0400

    Adding router to the react project

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
+import Header from './Layout/Header';
 
-export default class Dashboard extends Component {
+
+ class Dashboard extends Component {
     render() {
         return (
             <div>
-                
+            <h1 className ="alert alert-warning">Welcome to DashBoard</h1>
+            <ProjectItem/>
+            <Header/>
             </div>
-        )
+        );
     }
 }
+
+export default Dashboard;
