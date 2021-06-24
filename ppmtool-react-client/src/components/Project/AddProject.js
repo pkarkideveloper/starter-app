a34023585bd426804e6852e0e8c7449c83889af3
unknown <pkarki.developer@gmail.com>
2021-06-24 08:42:10 -0400

    adding project form

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -4,12 +4,29 @@ import React, { Component } from 'react'
      constructor(){
          super();
          this.state ={
-             "projectName":"react project",
-              "projectIdentifier":"ReactJs",
-              "description":"desc",
-              "startDate":"2021-03-01",
-              "targetDate":"2021-07-23"
-         }
+             projectName:"",
+              projectIdentifier:"",
+              description:"",
+              startDate:"",
+              targetDate:""
+         }; 
+         this.onChange = this.onChange.bind(this);
+         this.onSubmit = this.onSubmit(this);
+     }
+     onChange(e){
+         this.setState({[e.target.name]:e.target.value});
+     }
+
+     onSubmit(e){
+       
+         const newProject =  {
+            projectName: this.projectName,
+             projectIdentifier:this.projectIdentifier,
+             description:this.description,
+             startDate:this.startDate,
+             targetDate:this.targetDate
+        };
+        console.log(newProject);
      }
     render() {
         return (
@@ -19,28 +36,44 @@ import React, { Component } from 'react'
                 <div className="col-md-8 m-auto">
                     <h5 className="display-4 text-center">Create / Edit Project form</h5>
                     <hr />
-                    <form>
+                    <form onSubmit ={this.onSubmit}>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg " placeholder="Project Name"  name = "projectName"/>
+                            <input type="text" className="form-control form-control-lg " placeholder="Project Name"  
+                            name = "projectName"
+                            value = {this.state.projectName}
+                            onChange ={this.onChange.bind(this)}/>
                         </div>
                         <div className="form-group">
                             <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID"
                             name = "projectIdentifier"
+                            value = {this.state.projectIdentifier}
+                            onChange ={this.onChange.bind(this)}
+
                                  />
                         </div>
                        {
                            // <!-- disabled for Edit Only!! remove "disabled" for the Create operation -->
                        }
                         <div className="form-group">
-                            <textarea className="form-control form-control-lg" placeholder="Project Description" name ="description"></textarea>
+                            <textarea className="form-control form-control-lg" placeholder="Project Description"
+                             name ="description"
+                             value = {this.state.description}
+                             onChange ={this.onChange.bind(this)}></textarea>
                         </div>
                         <h6>Start Date</h6>
                         <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="startDate" />
+                            <input type="date" className="form-control form-control-lg" 
+                            name="startDate"
+                            value = {this.state.startDate} 
+                            onChange ={this.onChange.bind(this)}/>
+
                         </div>
                         <h6>Estimated End Date</h6>
                         <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="targetDate" />
+                            <input type="date" className="form-control form-control-lg" name="targetDate"
+                            value = {this.state.targetDate} 
+                            onChange ={this.onChange.bind(this)}/>
+
                         </div>
 
                         <input type="submit" className="btn btn-primary btn-block mt-4" />
